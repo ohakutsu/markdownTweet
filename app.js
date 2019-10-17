7a5cebbe9eabceb09b7727ae74d8217f0446862f
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-10-17 16:16:01 +0900

    [update] Bootstrapの追加

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

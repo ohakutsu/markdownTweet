b10e36eeb7f102ea1d0a12714631c701415ef950
TakuyaHanada <takuyahanada.hntk@gmail.com>
2019-10-19 00:56:15 +0900

    [update] ページネーション機能の追加

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,18 @@ const db = require('../models');
 const router = express.Router();
 const markdown = new MarkdownIt();
 
-router.get('/', async (req, res, next) => {
+const pagenate = ({ page, pageSize }) => {
+  const offset = page * pageSize;
+  const limit = pageSize + 1;
+  return { offset, limit };
+};
+
+router.get('/:page', async (req, res, next) => {
+  let { page } = req.params;
+  if (page < 1) page = 1;
+  page -= 1;
+  const pageSize = 10;
+
   const items = await db.item.findAll({
     order: [
       ['createdAt', 'DESC'],
@@ -14,9 +25,20 @@ router.get('/', async (req, res, next) => {
       model: db.user,
       require: true,
     },
+    ...pagenate({ page, pageSize }),
   });
 
-  res.render('home', { items, title: 'Home', markdown });
+  const hasNextPage = items.length > pageSize;
+  const resItems = hasNextPage ? items.slice(0, -1) : items;
+  res.render('home', {
+    items: resItems,
+    title: 'Home',
+    markdown,
+    pagenate: {
+      page: page + 1,
+      hasNextPage,
+    },
+  });
 });
 
 module.exports = router;
